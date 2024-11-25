264c73128d4f3e682a66c41e7ea3b8226e010370
NepyAnna <nepyann@gmail.com>
2024-11-25 21:25:05 +0100

    refactoring

--- src/test/java/com/sheoanna/PlanetTypeTest.java
+++ src/test/java/com/sheoanna/PlanetTypeTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 public class PlanetTypeTest {
     @Test
     void testFromMass() {
-        double mass = 2.5E6;
+        double mass = 2.5E26;
         assertEquals(PlanetType.GAS, PlanetType.fromMass(mass));
 
-        mass = 1.5E6;
+        mass = 1.5E23;
         assertEquals(PlanetType.TERRESTRIAL, PlanetType.fromMass(mass));
 
-        mass = 5E5;
+        mass = 1.1E21;
         assertEquals(PlanetType.DWARF, PlanetType.fromMass(mass));
 
         mass = -1000000;
